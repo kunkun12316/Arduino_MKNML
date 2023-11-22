4fdd72a6ff60d0dfa4896a96a2e64ec5407f164f
kunkun12316 <f13780062396@gmail.com>
2023-11-22 20:41:24 +0800

    在原本的基础上增加PID调节转速(还有问题)

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,18 @@
 #include "pid.h"
 
 pid_calibration PID_data = {
-    .kp = 0.01, // Proportional gain
-    .ki = 0.01, // Integral gain
-    .kd = 0.01  // Derivative gain
+    .kp = 0, // Proportional gain
+    .ki = 0, // Integral gain
+    .kd = 0  // Derivative gain
 };
 
 pid_state PID_status = {
-    .actual = 0.01,         // 测量的实际值
-    .target = 0.01,         // 期望的值
-    .time_delta = 0.01,     // 更新状态时应设置自上次采样/计算以来的时间
-    .previous_error = 0.01, // 先前计算的实际与目标之间的误差(初始为零)
-    .integral = 0.01,       // 积分误差随时间的总和
-    .output = 0.01          // 修正后的输出值由算法计算，对误差进行补偿
+    .actual = 0,         // 测量的实际值
+    .target = 0,         // 期望的值
+    .time_delta = 0,     // 更新状态时应设置自上次采样/计算以来的时间
+    .previous_error = 0, // 先前计算的实际与目标之间的误差(初始为零)
+    .integral = 0,       // 积分误差随时间的总和
+    .output = 0          // 修正后的输出值由算法计算，对误差进行补偿
 };
 
 // 定义电机控制引脚
@@ -41,19 +41,19 @@ double R1_ps = Car_ps;
 double L2_ps = Car_ps;
 double R2_ps = Car_ps;
 
-const int L1_Max = 7000;
-const int R1_Max = 7280;
-const int L2_Max = 7015;
-const int R2_Max = 7100;
+const short L1_Max = 7000;
+const short R1_Max = 7280;
+const short L2_Max = 7015;
+const short R2_Max = 7100;
 
-volatile long L1_encoderPos = 0;
-volatile long L1_last_encoderPos = 0;
-volatile long R1_encoderPos = 0;
-volatile long R1_last_encoderPos = 0;
-volatile long L2_encoderPos = 0;
-volatile long L2_last_encoderPos = 0;
-volatile long R2_encoderPos = 0;
-volatile long R2_last_encoderPos = 0;
+volatile double L1_encoderPos = 0;
+volatile double L1_last_encoderPos = 0;
+volatile double R1_encoderPos = 0;
+volatile double R1_last_encoderPos = 0;
+volatile double L2_encoderPos = 0;
+volatile double L2_last_encoderPos = 0;
+volatile double R2_encoderPos = 0;
+volatile double R2_last_encoderPos = 0;
 
 // 四个电机的平均速度
 double middle_speed = 0;
@@ -82,6 +82,190 @@ bool L2_backward_Status = false;
 bool R2_forward_Status = false;
 bool R2_backward_Status = false;
 bool allstop_Status = false;
+
+double PID_output(short encoderPos_Max, double middle_encoderPos, double new_encoderPos)
+{
+  // PID_data.kp = 255.0 / encoderPos_Max;
+  // PID_data.ki = 0;
+  // PID_data.kd = 0;
+
+  // PID_status.actual = new_encoderPos;
+  // PID_status.target = middle_encoderPos;
+  // PID_status.time_delta = 0.3; 
+
+  // pid_iterate(PID_data, PID_status);
+  // return PID_status.output;
+
+  double PID_out = 255.0 / encoderPos_Max * (middle_encoderPos - new_encoderPos) + 0.3 * (middle_encoderPos - new_encoderPos);
+
+  return PID_out;
+}
+
+double Judge_data(double data)
+{
+  if (data >= ps_Max)
+  {
+    data = ps_Max;
+  }
+  else if (data <= ps_Min)
+  {
+    data = ps_Min;
+  }
+  return data;
+}
+
+// 比较函数，用于在 qsort 中比较两个元素
+int compare(const void *a, const void *b)
+{
+  return (*(int *)a - *(int *)b);
+}
+
+void PIDTask(void *pvParameters)
+{
+  while (1)
+  {
+    double speeds[] = {L1_speed, R1_speed, L2_speed, R2_speed};
+    double size = sizeof(speeds) / sizeof(speeds[0]);
+
+    // 使用 qsort 函数进行升序排序
+    qsort(speeds, size, sizeof(speeds[0]), compare);
+
+    // 中间速度是排序后数组的第三个元素
+    middle_speed = speeds[2];
+
+    L1_ps = L1_ps + PID_output(L1_Max, middle_speed, L1_speed);
+    R1_ps = R1_ps + PID_output(R1_Max, middle_speed, R1_speed);
+    L2_ps = L2_ps + PID_output(L2_Max, middle_speed, L2_speed);
+    R2_ps = R2_ps + PID_output(R2_Max, middle_speed, R2_speed);
+
+    L1_ps = Judge_data(L1_ps);
+    R1_ps = Judge_data(R1_ps);
+    L2_ps = Judge_data(L2_ps);
+    R2_ps = Judge_data(R2_ps);
+
+    Serial.print(L1_ps);
+    Serial.print(" ");
+    Serial.print(R1_ps);
+    Serial.print(" ");
+    Serial.print(L2_ps);
+    Serial.print(" ");
+    Serial.println(R2_ps);
+
+    Serial.print(L1_speed);
+    Serial.print(" ");
+    Serial.print(R1_speed);
+    Serial.print(" ");
+    Serial.print(L2_speed);
+    Serial.print(" ");
+    Serial.println(R2_speed);
+
+    vTaskDelay(pdMS_TO_TICKS(300));
+  }
+}
+
+// void testTask(void *pvParameters) {
+//     while (1)
+//     {
+
+//       // Serial.print(L1_speed);
+//       // Serial.print(" ");
+//       // Serial.print(R1_speed);
+//       // Serial.print(" ");
+//       // Serial.print(L2_speed);
+//       // Serial.print(" ");
+//       // Serial.println(R2_speed);
+
+//       // Serial.print(L1_ps);
+//       // Serial.print(" ");
+//       // Serial.print(R1_ps);
+//       // Serial.print(" ");
+//       // Serial.print(L2_ps);
+//       // Serial.print(" ");
+//       // Serial.println(R2_ps);
+//       vTaskDelay(pdMS_TO_TICKS(50));
+//     }
+// }
+
+void motor_Task(void *pvParameters)
+{
+  while (1)
+  {
+    if (L1_forward_Status)
+    {
+      // 左前轮前进
+      analogWrite(L1_IN1, L1_ps);
+      digitalWrite(L1_IN2, LOW);
+    }
+
+    if (L1_backward_Status)
+    {
+      // 左前轮后退
+      digitalWrite(L1_IN1, LOW);
+      analogWrite(L1_IN2, L1_ps);
+    }
+
+    if (R1_forward_Status)
+    {
+      // 右前轮前进
+      analogWrite(R1_IN1, R1_ps);
+      digitalWrite(R1_IN2, LOW);
+    }
+
+    if (R1_backward_Status)
+    {
+      digitalWrite(R1_IN1, LOW);
+      analogWrite(R1_IN2, R1_ps);
+    }
+
+    if (L2_forward_Status)
+    { // 左后轮前进
+      analogWrite(L2_IN1, L2_ps);
+      digitalWrite(L2_IN2, LOW);
+    }
+
+    if (L2_backward_Status)
+    {
+      // 左后轮后退
+      digitalWrite(L2_IN1, LOW);
+      analogWrite(L2_IN2, L2_ps);
+    }
+
+    if (R2_forward_Status)
+    { // 右后轮前进
+      analogWrite(R2_IN1, R2_ps);
+      digitalWrite(R2_IN2, LOW);
+    }
+
+    if (R2_backward_Status)
+    { // 右后轮后退
+      digitalWrite(R2_IN1, LOW);
+      analogWrite(R2_IN2, R2_ps);
+    }
+
+    if (allstop_Status)
+    {
+      digitalWrite(L1_IN1, LOW);
+      digitalWrite(L1_IN2, LOW);
+      digitalWrite(R1_IN1, LOW);
+      digitalWrite(R1_IN2, LOW);
+      digitalWrite(L2_IN1, LOW);
+      digitalWrite(L2_IN2, LOW);
+      digitalWrite(R2_IN1, LOW);
+      digitalWrite(R2_IN2, LOW);
+
+      L1_forward_Status = false;
+      L1_backward_Status = false;
+      R1_forward_Status = false;
+      R1_backward_Status = false;
+      L2_forward_Status = false;
+      L2_backward_Status = false;
+      R2_forward_Status = false;
+      R2_backward_Status = false;
+    }
+
+    vTaskDelay(pdMS_TO_TICKS(200));
+  }
+}
 
 void moveTask(void *pvParameters)
 {
@@ -187,227 +371,6 @@ void moveTask(void *pvParameters)
   }
 }
 
-int PID_output(long encoderPos_Max, double middle_encoderPos, long new_encoderPos)
-{
-  int PID_out = 255.0 / encoderPos_Max * (middle_encoderPos - new_encoderPos);
-  return PID_out;
-}
-
-double Judge_data(double data)
-{
-  if (data >= ps_Max)
-  {
-    data = ps_Max;
-  }
-  else if (data <= ps_Min)
-  {
-    data = ps_Min;
-  }
-  return data;
-}
-
-// 比较函数，用于在 qsort 中比较两个元素
-int compare(const void *a, const void *b)
-{
-  return (*(int *)a - *(int *)b);
-}
-
-void PIDTask(void *pvParameters)
-{
-  while (1)
-  {
-    double speeds[] = {L1_speed, R1_speed, L2_speed, R2_speed};
-    double size = sizeof(speeds) / sizeof(speeds[0]);
-
-    // 使用 qsort 函数进行升序排序
-    qsort(speeds, size, sizeof(speeds[0]), compare);
-
-    // 中间速度是排序后数组的第三个元素
-    middle_speed = speeds[2];
-
-    L1_ps = L1_ps + PID_output(L1_Max, middle_speed, L1_speed);
-    R1_ps = R1_ps + PID_output(R1_Max, middle_speed, R1_speed);
-    L2_ps = L2_ps + PID_output(L2_Max, middle_speed, L2_speed);
-    R2_ps = R2_ps + PID_output(R2_Max, middle_speed, R2_speed);
-
-    L1_ps = Judge_data(L1_ps);
-    R1_ps = Judge_data(R1_ps);
-    L2_ps = Judge_data(L2_ps);
-    R2_ps = Judge_data(R2_ps);
-
-    Serial.print(L1_ps);
-    Serial.print(" ");
-    Serial.print(R1_ps);
-    Serial.print(" ");
-    Serial.print(L2_ps);
-    Serial.print(" ");
-    Serial.println(R2_ps);
-
-    Serial.print(L1_speed);
-    Serial.print(" ");
-    Serial.print(R1_speed);
-    Serial.print(" ");
-    Serial.print(L2_speed);
-    Serial.print(" ");
-    Serial.println(R2_speed);
-
-    vTaskDelay(pdMS_TO_TICKS(2000));
-  }
-}
-
-// void testTask(void *pvParameters) {
-//     while (1)
-//     {
-
-//       // Serial.print(L1_speed);
-//       // Serial.print(" ");
-//       // Serial.print(R1_speed);
-//       // Serial.print(" ");
-//       // Serial.print(L2_speed);
-//       // Serial.print(" ");
-//       // Serial.println(R2_speed);
-
-//       // Serial.print(L1_ps);
-//       // Serial.print(" ");
-//       // Serial.print(R1_ps);
-//       // Serial.print(" ");
-//       // Serial.print(L2_ps);
-//       // Serial.print(" ");
-//       // Serial.println(R2_ps);
-//       vTaskDelay(pdMS_TO_TICKS(50));
-//     }
-// }
-
-void L1_forward_Task(void *pvParameters)
-{
-  while (1)
-  {
-    if (L1_forward_Status)
-    {
-      // 左前轮前进
-
-      analogWrite(L1_IN1, L1_ps);
-      digitalWrite(L1_IN2, LOW);
-    }
-    vTaskDelay(pdMS_TO_TICKS(10));
-  }
-}
-
-void L1_backward_Task(void *pvParameters)
-{
-  while (1)
-  {
-    if (L1_backward_Status)
-    {
-      // 左前轮后退
-
-      digitalWrite(L1_IN1, LOW);
-      analogWrite(L1_IN2, L1_ps);
-    }
-    vTaskDelay(pdMS_TO_TICKS(10));
-  }
-}
-
-void R1_forward_Task(void *pvParameters)
-{
-  while (1)
-  {
-    if (R1_forward_Status)
-    {
-      // 右前轮前进
-
-      analogWrite(R1_IN1, R1_ps);
-      digitalWrite(R1_IN2, LOW);
-    }
-    vTaskDelay(pdMS_TO_TICKS(10));
-  }
-}
-
-void R1_backward_Task(void *pvParameters)
-{
-  while (1)
-  {
-    if (R1_backward_Status)
-    {
-      digitalWrite(R1_IN1, LOW);
-      analogWrite(R1_IN2, R1_ps);
-    }
-    vTaskDelay(pdMS_TO_TICKS(10));
-  }
-}
-
-void L2_forward_Task(void *pvParameters)
-{
-  while (1)
-  {
-    if (L2_forward_Status)
-    { // 左后轮前进
-      analogWrite(L2_IN1, L2_ps);
-      digitalWrite(L2_IN2, LOW);
-    }
-    vTaskDelay(pdMS_TO_TICKS(10));
-  }
-}
-
-void L2_backward_Task(void *pvParameters)
-{
-  while (1)
-  {
-    if (L2_backward_Status)
-    {
-      // 左后轮后退
-      digitalWrite(L2_IN1, LOW);
-      analogWrite(L2_IN2, L2_ps);
-    }
-    vTaskDelay(pdMS_TO_TICKS(10));
-  }
-}
-
-void R2_forward_Task(void *pvParameters)
-{
-  while (1)
-  {
-    if (R2_forward_Status)
-    {
-      analogWrite(R2_IN1, R2_ps);
-      digitalWrite(R2_IN2, LOW);
-    }
-    vTaskDelay(pdMS_TO_TICKS(10));
-  }
-}
-
-void R2_backward_Task(void *pvParameters)
-{
-  while (1)
-  {
-    if (R2_backward_Status)
-    {
-      digitalWrite(R2_IN1, LOW);
-      analogWrite(R2_IN2, R2_ps);
-    }
-    vTaskDelay(pdMS_TO_TICKS(10));
-  }
-}
-
-void allstop_Task(void *pvParameters)
-{
-  while (1)
-  {
-    if (allstop_Status)
-    {
-      digitalWrite(L1_IN1, LOW);
-      digitalWrite(L1_IN2, LOW);
-      digitalWrite(R1_IN1, LOW);
-      digitalWrite(R1_IN2, LOW);
-      digitalWrite(L2_IN1, LOW);
-      digitalWrite(L2_IN2, LOW);
-      digitalWrite(R2_IN1, LOW);
-      digitalWrite(R2_IN2, LOW);
-    }
-    vTaskDelay(pdMS_TO_TICKS(10));
-  }
-}
-
 void L1_doEncoder()
 {
   // 计算速度
@@ -509,18 +472,10 @@ void R2_doEncoder()
 
 void setup()
 {
-  xTaskCreate(allstop_Task, "通过PID+编码器检测速度", 1000, NULL, 1, NULL);
   xTaskCreate(PIDTask, "通过PID+编码器检测速度", 1000, NULL, 2, NULL);
   // xTaskCreate(testTask, "通过编码器检测速度", 1000, NULL, 3, NULL);
-  xTaskCreate(moveTask, "电机动作", 1000, NULL, 3, NULL);
-  xTaskCreate(L1_forward_Task, "通过PID+编码器检测速度", 1000, NULL, 3, NULL);
-  xTaskCreate(L1_backward_Task, "通过PID+编码器检测速度", 1000, NULL, 3, NULL);
-  xTaskCreate(R1_forward_Task, "通过PID+编码器检测速度", 1000, NULL, 3, NULL);
-  xTaskCreate(R1_backward_Task, "通过PID+编码器检测速度", 1000, NULL, 3, NULL);
-  xTaskCreate(L2_forward_Task, "通过PID+编码器检测速度", 1000, NULL, 3, NULL);
-  xTaskCreate(L2_backward_Task, "通过PID+编码器检测速度", 1000, NULL, 3, NULL);
-  xTaskCreate(R2_forward_Task, "通过PID+编码器检测速度", 1000, NULL, 3, NULL);
-  xTaskCreate(R2_backward_Task, "通过PID+编码器检测速度", 1000, NULL, 3, NULL);
+  xTaskCreate(moveTask, "电机动作", 1000, NULL, 1, NULL);
+  xTaskCreate(motor_Task, "改变电机状态", 1000, NULL, 3, NULL);
 
   attachInterrupt(digitalPinToInterrupt(L1_encoderPin), L1_doEncoder, CHANGE);
   attachInterrupt(digitalPinToInterrupt(R1_encoderPin), R1_doEncoder, CHANGE);
